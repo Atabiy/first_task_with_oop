2f186f53f93af17d32790b830bdca8bcc2223d57
Atabiy <tnikita17@gmail.com>
2021-08-28 18:25:44 +0300

    New version

--- form.php
+++ form.php
@@ -1,4 +1,15 @@
-<form action="main.php" method="post">
-    Введите числа:  <input type="text" name="digits" /><br/>
-    <input type="submit" name="submit" value="Отправить числа на сортирoвку!" />
-</form>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My_First_Homework</title>
+        <meta char = UTF-8>
+    </head>
+    <body>
+        <h1>Поиск дубликатов среди чисел<h2>
+        <h2>Введите числа, которые вы хотите отсортировать, в форму ниже, через пробел.
+        <form action="main.php" method="post">
+            Введите числа:  <input type="text" name="digits" /><br/>
+            <input type="submit" name="submit" value="Отправить числа на сортирoвку!" />
+        </form>
+    </body>
+</html>
--- func.php
+++ func.php
@@ -1,6 +1,9 @@
 <?php
 function finder($dg){
     for ($i = 0; i < strlen($dg) - 1; ++$i){
+        if ($dg[$i] == ' '){
+            continue;
+        }
         if (substr_count($dg, $dg[$i]) > 1){
             return $dg[$i];
             break;}
