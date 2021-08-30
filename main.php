1a7f311496694d109805e501e3c4120dae1f8bdc
Atabiy <tnikita17@gmail.com>
2021-08-30 16:50:17 +0300

    This improved version still doesn't work

--- main.php
+++ main.php
@@ -3,11 +3,9 @@ require('class.php');
 $digits = $_POST['digits'];
 $answer = new finder;
 $answer -> find_dublicate($digits);
-$a = $answer -> answer;
-echo $a
-/*if (($answer -> answer) != ''){ 
+if (($answer -> answer) != ''){ 
     echo "У числа <strong> '$answer -> answer' </strong> есть дубликаты!";}
 else{
     echo 'Дубликатов нету!';
-}*/
+}
 ?>
